e0b61b1b06d45ef2aff3f76f2fa3da9ef2ff6340
Mahan Kavian <kavianmahandvp@gmail.com>
2024-03-05 14:08:26 +0330

    improve the basket page and contact, create checkout page

---
+++ src/mock/checkOutBasket.ts
@@ -0,0 +1,25 @@
+const checkOutBasket = [
+    {
+        image: "/assets/images/burger-p-1.png",
+        title: "Exotic delicious",
+        weight: "0.5kg",
+        price: "8.0"
+    },    {
+        image: "/assets/images/pizza-p-1.png",
+        title: "Exotic delicious",
+        weight: "0.5kg",
+        price: "18.0"
+    },    {
+        image: "/assets/images/cake-p-1.png",
+        title: "Exotic delicious",
+        weight: "0.5kg",
+        price: "10.25"
+    },    {
+        image: "/assets/images/burger-p-3.png",
+        title: "Exotic delicious",
+        weight: "0.5kg",
+        price: "14.75"
+    },
+];
+
+export default checkOutBasket;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,48 +3,50 @@ import {IconBox, ImageView, PagesNavigation, Section, SpecialBox} from "@/compon
 export default function contact() {
     return (
         <>
-            <PagesNavigation title={"Contact"} home={"Home"} next={"contact-page"}/>
-            <Section className="px-2">
-                <h1 className="text-2xl md:text-4xl text-center font-lobster m-3 lg:m-6">Contact us</h1>
+            <PagesNavigation title={"Contact"} home={"Home"} next={"Contact"}/>
+            <Section className="px-2 pb-8">
+                <h3 className="w-full text-center font-lobster font-medium font-Jost text-2xl md:text-4xl mb-4 lg:mb-8 text-silver-500">
+                    Contact us
+                </h3>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-2 my-2 md:my-4">
                     <div className="flex gap-4 items-center">
-                        <IconBox icon={"icon-contact-1"} size={40}/>
-                        <div>
-                            <p className="font-bold">Call us</p>
-                            <p className="text-sm">+45 987 368 7619</p>
+                        <IconBox icon={"icon-contact-1 text-[40px]"}/>
+                        <div className={'flex flex-col'}>
+                            <span className="font-medium text-silver-500">Call us</span>
+                            <span className="text-sm text-silver-300">+45 987 368 7619</span>
                         </div>
                     </div>
                     <div className="flex gap-4 items-center">
-                            <IconBox icon={"icon-contact-2"} size={40}/>
-                            <div>
-                                <p className="font-bold">Address</p>
-                                <p className="text-sm">29 Rue du Bourg Tibourg, 75004 Paris</p>
+                            <IconBox icon={"icon-contact-2 text-[40px]"}/>
+                            <div className={'flex flex-col'}>
+                                <span className="font-medium text-silver-500">Address</span>
+                                <span className="text-sm text-silver-300">29 Rue du Bourg Tibourg, 75004 Paris</span>
                             </div>
                     </div>
                     <div className="flex gap-4 items-center sm:hidden md:flex">
                             <IconBox icon={"icon-contact-3"} size={40}/>
-                            <div>
-                                <p className="font-bold">Email</p>
-                                <p className="text-sm">organicfood@email.com</p>
+                            <div className={'flex flex-col'}>
+                                <span className="font-medium text-silver-500">Email</span>
+                                <span className="text-sm text-silver-300">organicfood@email.com</span>
                             </div>
                     </div>
                 </div>
-                <ImageView src="/assets/images/map-contact.png" className="block w-full py-2 max-h-[500px]" alt={"pic"} width={500} height={500}/>
-                    <div className="flex flex-col sm:flex-row justify-between gap-4 py-4 sm:py-8">
-                        <div className="flex flex-col max-w-[350px] gap-3 sm:gap-6">
-                            <h2 className="text-2xl md:text-4xl font-lobster">Get in touch</h2>
-                            <p>If you got any question, please to not hesitate to send us a message. We reply within 24 hours!</p>
-                            <SpecialBox/>
-                        </div>
-                        <div className="max-w-[900px] flex-grow">
-                            <form className="flex gap-2 sm:gap-4 w-full flex-wrap" action="">
-                                <input type="text" placeholder="Name" className="bg-gray-100 text-black p-2 flex-grow md:max-w-[50%] outline-0"/>
-                                <input type="email" placeholder="Email" className="bg-gray-100 text-black p-2 flex-grow md:max-w-[50%] outline-0" />
-                                <textarea placeholder="Message" className="block w-full h-[100px] flex-grow-0 bg-gray-100 text-black p-2 resize-none outline-0"></textarea>
-                                <button className="w-full text-center lg:text-lg tracking-[5px] font-[600] p-2 text-black bg-gray-300 hover:bg-black hover:text-white transition-all duration-200" type="submit">SUBMIT</button>
-                            </form>
-                        </div>
+                <ImageView src="/assets/images/map-contact.png" className="block w-full py-2 max-h-[500px] mb-5" alt={"pic"} width={500} height={500}/>
+                <div className="flex flex-col sm:flex-row justify-between gap-4 py-4 sm:py-8">
+                    <div className="flex flex-col max-w-[350px] gap-3 sm:gap-6">
+                        <h2 className="text-2xl md:text-4xl font-medium text-silver-500 font-lobster">Get in touch</h2>
+                        <p className={"text-silver-300 font-normal"}>If you got any question, please to not hesitate to send us a message. We reply within 24 hours!</p>
+                        <SpecialBox/>
                     </div>
+                    <div className="max-w-[900px] flex-grow">
+                        <form className="flex gap-2 sm:gap-4 w-full flex-wrap" action="">
+                            <input type="text" placeholder="Name" className="bg-silver-100 text-black p-2 flex-grow md:max-w-[50%] outline-none placeholder:text-sm"/>
+                            <input type="email" placeholder="Email" className="bg-silver-100 text-black p-2 flex-grow md:max-w-[50%] outline-none" />
+                            <textarea placeholder="Message" className="block w-full h-[100px] flex-grow-0 bg-silver-100 text-black p-2 resize-none outline-none"></textarea>
+                            <button className="w-full text-center lg:text-medium tracking-[2px] text-md p-2 bg-silver-500 hover:bg-primary-300 text-white transition" type="submit">SUBMIT</button>
+                        </form>
+                    </div>
+                </div>
             </Section>
         </>
     );
