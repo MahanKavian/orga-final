dd9e3ff605c99bd0de3519bf58e7af78bbfdd2ec
Mahan Kavian <kavianmahandvp@gmail.com>
2024-03-23 12:25:30 +0330

    edit header function

--- src/components/api/Menu.ts
+++ src/components/api/Menu.ts
@@ -3,10 +3,9 @@ import {ResponseMenuType} from "@/types/api/MenuResponseType";
 
 
 export async function getApiMenu():Promise<Array<ResponseMenuType>>{
-    const response = await apiClient.get("/menus",{
+    return await apiClient.get("/menus",{
         params:{
             populate: "*",
         }
     })
-    return response.data
 }
--- src/utils/filterMenuPositions.ts
+++ src/utils/filterMenuPositions.ts
@@ -4,9 +4,10 @@ interface Props{
     position: string
 }
 export const FilterMenuPositions = ({mainMenuData, position}: Props) => {
+    console.log(mainMenuData)
     let mainMenuItems = []
     if(mainMenuData){
-        const filteredMenu: any = mainMenuData.filter((item:any)=> item.attributes.position === position)
+        const filteredMenu: any = mainMenuData.data.filter((item:any)=> item.attributes.position === position);
         if(filteredMenu.length > 0 ){
             mainMenuItems.push(filteredMenu[0].attributes)
             mainMenuItems = mainMenuItems[0].menu_items.data
