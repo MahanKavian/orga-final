c6ba21aff968af9eeb067fcd9371cb5220ba115c
Mahan Kavian <kavianmahandvp@gmail.com>
2024-04-18 12:58:17 +0330

    edit hero detail and update Rating component

--- src/components/common/products/product-cards/MainProductCard.tsx
+++ src/components/common/products/product-cards/MainProductCard.tsx
@@ -19,7 +19,7 @@ export function MainProductCard({data}: Props) {
             </div>
             <div className="flex flex-col gap-2 justify-center items-center py-8 px-5">
                 <span className={"capitalize text-silver-300"}>{data.category.data.attributes.title}</span>
-                <Rating rate={data.rate ? data.rate : 5.0} hideText={false}/>
+                <Rating rate={data.rate ? data.rate : 5.0} hideText={true}/>
                 <span className="text-lg text-silver-500 font-normal text-center first-letter:uppercase text-dark">{data.title}</span>
                 <PriceText price={data.price} sale_price={data.sale_price} centerText={true}/>
             </div>
