3e6fc273cfd69ab8649eb1e677558848e4c4aa56
AliAeini <145900202+AliAeini@users.noreply.github.com>
2024-04-19 16:25:04 +0330

    connect SearchForm.tsx to api

---
+++ src/components/layouts/header/search_form/SearchForm.tsx
@@ -0,0 +1,91 @@
+import {IconBox, ImageView, PriceText} from "@/components";
+import {useForm} from "react-hook-form";
+import {useMutation} from "@tanstack/react-query";
+import {useEffect, useState} from "react";
+import {ProductType} from "@/types/api/Product";
+import {getAllProductApiCall} from "@/api/Products";
+import {EntityType, ResponseApi} from "@/types/api/ResponseApi";
+import useDebounce from "@/hooks/use-debounce";
+import Link from "next/link";
+import {useOverlay} from "@/hooks/use-overlay";
+
+interface Props{
+}
+interface formInput{
+    search_text: string
+}
+interface filterData{
+    title:{
+        "$containsi": string
+    }
+}
+export function SearchForm({}: Props) {
+    const [resultData, setResultData] = useState<Array<EntityType<ProductType>>>([])
+    const{register, handleSubmit, watch} = useForm<formInput>()
+    const mutationData = useMutation({mutationFn:(data:filterData)=> getAllProductApiCall({filters:data, populate:["thumbnail"]})})
+    const search_text = watch("search_text")
+    const [showResult, setShowResult] = useState(true)
+
+    useEffect(() => {
+        search_text && delay()
+        setShowResult(true)
+    }, [search_text]);
+
+    const onSubmit= (data:formInput)=>{
+        if(data.search_text.length <= 2 ){
+            return
+        }
+        mutationData.mutate({
+            title:{
+                "$containsi": data.search_text
+            }
+        },{
+            onSuccess: (response:ResponseApi<ProductType>)=>{
+                setResultData(response.data)
+            }
+        })
+    }
+    const delay = useDebounce(handleSubmit(onSubmit), 1000)
+
+    useOverlay({
+        onClick:()=>{
+            setShowResult(false)
+        }
+    })
+    const resultBody = (e:React.MouseEvent)=>{
+        e.stopPropagation()
+    }
+    return (
+        <div className={"relative min-w-[250px]"}>
+            <form onSubmit={handleSubmit(onSubmit)} name="search-form" action="#" method="post"
+                  className="w-full py-2 px-4 flex min-w-[200px] lg:min-w-[400px] items-center border-2 rounded-md">
+                <input autoComplete={undefined} type="text" {...register("search_text")} placeholder="Enyer your Keyword..." className="flex-grow focus:outline-none"/>
+                <button type={"submit"}>
+                    <IconBox icon={'icon-search-header text-[19px] hover:cursor-pointer hover:text-primary-200 transition'}/>
+                </button>
+            </form>
+            {
+                (resultData && showResult) &&
+                <div className="absolute top-14 left-0 w-full bg-white z-[60] rounded" onClick={resultBody}>
+                    <ul>
+                        {
+                            resultData.map((result:EntityType<ProductType>, index:number)=>{
+                                return(
+                                    <Link href={"#"} key={index}>
+                                        <li className="pr-2 rounded border border-light_gray hover:bg-lime-50 text-black hover:text-black cursor-pointer flex justify-between items-center">
+                                            <div className="flex gap-4 items-center">
+                                                <ImageView src={result.attributes.thumbnail.data.attributes.url} alt={result.attributes.description} width={55} height={55}/>
+                                                <p>{result.attributes.title}</p>
+                                            </div>
+                                            <PriceText price={result.attributes.price} sale_price={result.attributes.sale_price}/>
+                                        </li>
+                                    </Link>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+            }
+        </div>
+    );
+}
