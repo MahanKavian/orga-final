ab7216487d2ed3ab01151f6855f12c36ee17af61
AliAeini <145900202+AliAeini@users.noreply.github.com>
2024-08-09 17:21:56 +0330

    add inputs of checkout page to external component and connect to basket

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,71 +1,69 @@
 import Link from "next/link";
-import {IconBox, ImageView, PagesNavigation, Section} from "@/components";
-import checkOutBasket from "@/mock/checkOutBasket";
+import {CustomInput, IconBox, ImageView, PagesNavigation, Section} from "@/components";
+import {useContext} from "react";
+import {BasketCardContext} from "@/store/BasketCardContext";
+import {useForm} from "react-hook-form";
 
 interface Props {
 
 }
 
+interface formDataType{
+    email: string,
+    update: boolean,
+    first_name:string,
+    last_name: string,
+    address: string,
+    apartment: string
+    city: string,
+    postal_code: string,
+    country: string,
+    rules: boolean
+}
+
 export default function Checkout({}: Props) {
+    const {BasketItems, totalPrice} = useContext(BasketCardContext)
+    const {register, handleSubmit, formState:{errors}} = useForm<formDataType>();
+
+    const formSubmitHandler = (data: formDataType)=> {
+        console.log(data)
+    }
+
     return (
         <>
-            <PagesNavigation title={"Checkout"} home={"Home"} next={"Cechout"}/>
+            <PagesNavigation title={"Checkout"} home={"Home"} next={"Checkout"}/>
             <div className="w-full mb-[50px] py-10">
                 <Section className="p-3 md:py-3 md:px-0 flex flex-col items-start gap-16 lg:flex-row ">
                     <form action="#" className="w-full flex flex-col gap-5 lg:w-[55%]">
                         <div className="flex flex-col w-full gap-10">
                             <div className="flex flex-col gap-4">
                                 <div className="w-full flex justify-between gap-3 flex-col md:flex-row md:items-center">
-                                    <h3 className="font-medium capitalize text-silver-500 text-lg">Contact
-                                        information</h3>
-                                    <p className="text-xs font-medium text-silver-300">
-                                        Already have an account?
-                                        <Link href="#" className="text-primary-300">Log in</Link>
+                                    <h3 className="font-medium capitalize text-silver-500 text-lg">Contact information</h3>
+                                    <p className="text-sm font-medium text-silver-300">Already have an account?
+                                        <Link href="/login" className="text-primary-300"> Log in</Link>
                                     </p>
                                 </div>
-                                <input type="text" placeholder="Email or phone number"
-                                       className="py-2 px-4 bg-silver-100 placeholder:text-sm placeholder:font-[500] focus:outline-none"/>
+                                <CustomInput {...{placeholder: "Email or phone number"}} register={register("email", {required: "Email is Required"})} errors={errors} type={"email"}/>
                                 <div className="flex gap-3 items-center">
-                                    <input type="checkbox" id="update"/>
-                                    <label htmlFor="update" className="text-silver-300 font-medium text-xs">Keep me up
-                                        to
-                                        date on news and exclusive offers
-                                    </label>
+                                    <input {...register("update")} type="checkbox" id="update"/>
+                                    <label htmlFor="update" className="text-silver-300 font-medium text-xs">Keep me up to date on news and exclusive offers</label>
                                 </div>
                             </div>
-                            <div className="w-full flex flex-col gap-4">
-                                <h3 className="font-medium capitalize text-silver-500 text-lg mb-3">Shipping
-                                    address</h3>
-                                <div className="flex items-center gap-3 flex-wrap">
-                                    <input type="text" placeholder="First name"
-                                           className="flex-auto py-2 px-4 bg-silver-100 placeholder:text-sm placeholder:font-medium focus:outline-none"/>
-                                    <input type="text" placeholder="Last name"
-                                           className="flex-auto py-2 px-4 bg-silver-100 placeholder:text-sm placeholder:font-medium focus:outline-none"/>
-                                </div>
-                                <input type="text" placeholder="Address"
-                                       className="flex-auto py-2 px-4 bg-silver-100 placeholder:text-sm placeholder:font-medium focus:outline-none"/>
-                                <input type="text" placeholder="Apartment, suite, etc"
-                                       className="flex-auto py-2 px-4 bg-silver-100 placeholder:text-sm placeholder:font-medium focus:outline-none"/>
-                                <input type="text" placeholder="City"
-                                       className="flex-auto py-2 px-4 bg-silver-100 placeholder:text-sm placeholder:font-medium focus:outline-none"/>
-                                <div className="flex items-center gap-3 flex-wrap">
-                                    <input type="text" placeholder="Country/Region"
-                                           className="flex-auto py-2 px-4 bg-silver-100 placeholder:text-sm placeholder:font-medium focus:outline-none"/>
-                                    <input type="text" placeholder="Postal code"
-                                           className="flex-auto py-2 px-4 bg-silver-100 placeholder:text-sm placeholder:font-medium focus:outline-none"/>
-                                </div>
-                                <div className="flex items-center justify-between">
+                            <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4">
+                                <h3 className="col-span-full font-medium capitalize text-silver-500 text-lg mb-3">Shipping address</h3>
+                                <CustomInput {...{placeholder: "First name"}} register={register("first_name", {required: "FirstName Is Requierd"})} errors={errors}/>
+                                <CustomInput {...{placeholder: "Last name"}} register={register("last_name", {required: "LastName Is Requierd"})} errors={errors}/>
+                                <CustomInput parentClassName={"col-span-full"} {...{placeholder: "Address"}} register={register("address", {required: "Address Is Requierd"})} errors={errors}/>
+                                <CustomInput parentClassName={"col-span-full"} {...{placeholder: "Apartment, suite, etc"}} register={register("apartment", {required: "Apartment Is Requierd"})} errors={errors}/>
+                                <CustomInput parentClassName={"col-span-full"} {...{placeholder: "city"}} register={register("city", {required: "city is Requierd"})} errors={errors}/>
+                                <CustomInput {...{placeholder: "Postal code"}} register={register("postal_code", {required: "Postal Code Is Requierd"})} errors={errors}/>
+                                <CustomInput {...{placeholder: "Country/Region"}} register={register("country", {required: "Country Is Requierd"})} errors={errors}/>
+                                <div className="flex items-center justify-between col-span-full">
                                     <div className="flex gap-3 items-center">
-                                        <input type="checkbox" id="rules"/>
-                                        <label htmlFor="rules" className="text-silver-300 font-medium text-xs">Accept
-                                            the rules
-                                        </label>
+                                        <input {...register("rules")} type="checkbox" id="rules"/>
+                                        <label htmlFor="rules" className="text-silver-300 font-medium text-xs">Accept the rules</label>
                                     </div>
-                                    <Link href="#"
-                                          className="text-red font-medium text-sm flex gap-2 transition duration-200 items-center">
-                                        <IconBox icon={"icon-return"}/>
-                                        Return cart
-                                    </Link>
+                                    <IconBox icon={"icon-return"} title={"Return cart"} link={"/basket"} iconClassName={"text-red"} titleClassName={"text-red"}/>
                                 </div>
                             </div>
                         </div>
@@ -75,20 +73,18 @@ export default function Checkout({}: Props) {
                     <div className="w-full flex flex-col gap-8 lg:w-[30%]">
                         <div className="w-full flex flex-col h-[455px] gap-5 overflow-auto">
                             {
-                                checkOutBasket.map((item, index) => {
+                                BasketItems?.map((item, index) => {
                                     return (
-                                        <div className="w-full flex justify-between items-center" key={index}>
+                                        <Link href={{pathname: "/products/[id]", query: {id: item.productId}}} className="max-h-[90px] flex-grow p-2 flex justify-between items-center shadow shadow-gray-200" key={index}>
                                             <div className="flex gap-4 items-center">
-                                                <ImageView src={item.image} alt="" width={70}
-                                                           height={70}
-                                                           className="border border-silver-200"/>
+                                                <ImageView src={item.img!} alt="" width={70} height={70} className=""/>
                                                 <div className="flex flex-col gap-2">
                                                     <span className="font-medium text-silver-500 text-md">{item.title}</span>
-                                                    <span className="font-normal text-silver-300 text-sm">{item.weight}</span>
+                                                    <span className="font-normal text-silver-300 text-sm">{item.quantity} items</span>
                                                 </div>
                                             </div>
-                                            <span className="text-primary-300 font-normal text-md">${item.price}</span>
-                                        </div>
+                                            <span className="text-primary-300 font-normal text-md">{item.price * item.quantity} usd</span>
+                                        </Link>
                                     )
                                 })
                             }
@@ -97,7 +93,7 @@ export default function Checkout({}: Props) {
                         <div className="w-full flex flex-col gap-3">
                             <div className="w-full flex justify-between items-center">
                                 <span className="text-md font-normal text-silver-500">Subtotal</span>
-                                <span className="font-medium text-sm text-primary-300">$36.00</span>
+                                <span className="font-medium text-sm text-primary-300">{totalPrice} $</span>
                             </div>
                             <div className="w-full flex justify-between items-center">
                                 <span className="text-md font-normal text-silver-500">Shiping</span>
@@ -105,15 +101,15 @@ export default function Checkout({}: Props) {
                             </div>
                             <div className="w-full flex justify-between items-center">
                                 <span className="text-md font-normal text-silver-500">Taxes</span>
-                                <span className="font-medium text-sm text-primary-300">$3.60</span>
+                                <span className="font-medium text-sm text-primary-300">3.60 $</span>
                             </div>
                         </div>
                         <div className="h-[2px] w-full bg-silver-100"></div>
                         <div className="w-full flex justify-between items-center">
                             <span className="text-md font-normal text-gray">Total</span>
-                            <span className="font-medium text-sm text-primary-300"><span
-                                className="text-xs text-silver-300 font-semibold mr-1">USD</span> $39.60</span>
+                            <span className="font-medium text-gray-300"><span className="text-lg font-semibold mr-1 text-primary-300">{totalPrice}</span> USD</span>
                         </div>
+                        <button onClick={handleSubmit(formSubmitHandler)} type={"submit"} className="bg-primary-100 text-white p-2 w-full text-center tracking-[2px] transition hover:bg-primary-300">CONFIRM</button>
                     </div>
                 </Section>
             </div>
