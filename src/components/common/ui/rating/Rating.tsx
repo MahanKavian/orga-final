3795cea6b4827248ac95b660b2c90e221cf91398
AliAeini <145900202+AliAeini@users.noreply.github.com>
2024-08-11 14:04:04 +0330

    fix key warning in iconBox.tsx and rating.tsx

--- src/components/common/ui/rating/Rating.tsx
+++ src/components/common/ui/rating/Rating.tsx
@@ -10,11 +10,11 @@ export function Rating({rate, hideText}: Props) {
     const half_star = <li className="flex"><IconBox icon={"icon-star-half-empty"} iconClassName={"text-yellow"} size={12}/></li>
 
     for (let i = 0; i < Math.trunc(rate); i++) {
-        stars.push(<li className="flex"><IconBox icon={"icon-star"} iconClassName={"text-yellow"} size={12}/></li>)
+        stars.push(<li className="flex" key={i}><IconBox icon={"icon-star"} iconClassName={"text-yellow"} size={12}/></li>)
     }
     let emptyStars = []
     for (let i = Math.trunc(rate); i < (Number.isInteger(rate) ? 5 : 4); i++) {
-        emptyStars.push(<li className="flex"><IconBox icon={"icon-star-o"} iconClassName={"text-yellow"} size={12}/></li>)
+        emptyStars.push(<li className="flex" key={i}><IconBox icon={"icon-star-o"} iconClassName={"text-yellow"} size={12}/></li>)
     }
     return (
         <>
